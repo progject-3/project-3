28f477c2cef0afc06d974e098805f1dda972fa12
Zaid Fadel <zaid.fadel89@gmail.com>
2019-11-08 14:23:10 -0600

    map working now

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,20 +1,23 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import HomeMap from './HomeMap';
 class Home extends Component {
-
-
-    render() {
-        const imageStyle = {
-            width: 400
-        }
-        return (
-            <div>
-                <p>It's good to be home</p>
-                <img style={imageStyle} alt="home" src="https://i.ytimg.com/vi/N1icEHtgb3g/maxresdefault.jpg" />
-            </div>
-        )
-
-    }
+  render() {
+    const imageStyle = {
+      width: '50%',
+      height: '50%'
+    };
+    return (
+      <div>
+        <p>It's good to be home</p>
+        <img
+          style={imageStyle}
+          alt='home'
+          src='https://i.ytimg.com/vi/N1icEHtgb3g/maxresdefault.jpg'
+        />
+        <HomeMap />
+      </div>
+    );
+  }
 }
 
-export default Home
+export default Home;
