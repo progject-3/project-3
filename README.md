dc710b8056d13494b2f013ce019e7c6ec5058919
ahmed ahmed <algandel19@gmail.com>
2019-11-18 00:40:35 -0600

    readme

---
+++ README.md
@@ -0,0 +1,46 @@
+# Change the World
+
+[world](./image/1.png)
+
+   Our project is about rental apartments .
+Visiting City of Chicago and looking for Apartments that you like to stay in?
+Then this web application we provide is the best fit for you!
+Find your cozy Apartment to stay for the night.
+This Web application helps user finding the perfect rental apartments. Also you can find your next rental from search view in your targeted area anywhere in Chicago area.
+
+[Live Link]()
+
+## Login Page 
+
+![login](./image/2.png)
+
+## Sign Up Page
+
+![sign up](./image/3.png)
+
+## Main Page
+
+![Main](./image/4.png)
+
+## Specific Page
+
+![Specific](./image/5.png)
+
+## Technologies used:
+
+   * React
+   * React Helmet
+   * React Bootstrap
+   * Font Awesome
+   * Googel Map Api
+   * Airbnb Api
+   * Node
+   * Express
+   * MongoDB
+   * Mongoose ORM
+   * Sequelize
+   * Body Parser
+   * Passport
+
+## Functionality:
+
--- client/src/components/Calendar/Calendar.js
+++ client/src/components/Calendar/Calendar.js
@@ -1,30 +1,60 @@
 import React from 'react';
+
 import Helmet from 'react-helmet';
+
 import DayPicker, { DateUtils } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 
 export default class Example extends React.Component {
-  static defaultProps = {
-    numberOfMonths: 2,
-  };
-
   constructor(props) {
     super(props);
     this.handleDayClick = this.handleDayClick.bind(this);
+    this.handleDayMouseEnter = this.handleDayMouseEnter.bind(this);
     this.handleResetClick = this.handleResetClick.bind(this);
     this.state = this.getInitialState();
   }
 
   getInitialState() {
     return {
-      from: undefined,
-      to: undefined,
+      from: null,
+      to: null,
+      enteredTo: null, // Keep track of the last day for mouseEnter.
     };
   }
 
+  isSelectingFirstDay(from, to, day) {
+    const isBeforeFirstDay = from && DateUtils.isDayBefore(day, from);
+    const isRangeSelected = from && to;
+    return !from || isBeforeFirstDay || isRangeSelected;
+  }
+
   handleDayClick(day) {
-    const range = DateUtils.addDayToRange(day, this.state);
-    this.setState(range);
+    const { from, to } = this.state;
+    if (from && to && day >= from && day <= to) {
+      this.handleResetClick();
+      return;
+    }
+    if (this.isSelectingFirstDay(from, to, day)) {
+      this.setState({
+        from: day,
+        to: null,
+        enteredTo: null,
+      });
+    } else {
+      this.setState({
+        to: day,
+        enteredTo: day,
+      });
+    }
+  }
+
+  handleDayMouseEnter(day) {
+    const { from, to } = this.state;
+    if (!this.isSelectingFirstDay(from, to, day)) {
+      this.setState({
+        enteredTo: day,
+      });
+    }
   }
 
   handleResetClick() {
@@ -32,65 +62,71 @@ export default class Example extends React.Component {
   }
 
   render() {
-    const { from, to } = this.state;
-    const modifiers = { start: from, end: to };
-    return (
-      <div className="RangeExample">
-        <p style={{
-          color: "white"
-        }}> 
-          {!from && !to && 'Please select your first day of stay.'}
-          {from && !to && 'Please select the last day of your stay.'}
-          {from &&
-            to &&
-            `Selected from ${from.toLocaleDateString()} to
-                ${to.toLocaleDateString()}`}{' '}
-          {from && to && (
-            <button className="link" onClick={this.handleResetClick}>
-              Reset
-            </button>
-          )}
-        </p>
-        
-        <DayPicker 
-          className="Selectable"
-          numberOfMonths={this.props.numberOfMonths}
-          selectedDays={[from, { from, to }]}
+    const { from, to, enteredTo } = this.state;
+    const modifiers = { start: from, end: enteredTo };
+    const disabledDays = { before: this.state.from };
+    const selectedDays = [from, { from, to: enteredTo }];
+   
+      return (
+        <div className="RangeExample">
+          <p style={{
+            color: "white"
+          }}> 
+            {!from && !to && 'Please select your first day of stay.'}
+            {from && !to && 'Please select the last day of your stay.'}
+            {from &&
+              to &&
+              `Selected from ${from.toLocaleDateString()} to
+                  ${to.toLocaleDateString()}`}{' '}
+            {from && to && (
+              <button className="link" onClick={this.handleResetClick}>
+                Reset
+              </button>
+            )}
+          </p>
+          
+          <DayPicker
+          className="Range"
+          numberOfMonths={1}
+          fromMonth={from}
+          selectedDays={selectedDays}
+          disabledDays={disabledDays}
           modifiers={modifiers}
           onDayClick={this.handleDayClick}
-          />
-        <Helmet>
-          <style>{`
-  .Selectable .DayPicker-Day--selected:not(.DayPicker-Day--start):not(.DayPicker-Day--end):not(.DayPicker-Day--outside) {
-    background-color: #f0f8ff !important;
-    color: #4a90e2;
+          onDayMouseEnter={this.handleDayMouseEnter}
+        />
+          <Helmet>
+            <style>{`
+    .Selectable .DayPicker-Day--selected:not(.DayPicker-Day--start):not(.DayPicker-Day--end):not(.DayPicker-Day--outside) {
+      background-color: #f0f8ff !important;
+      color: #4a90e2;
+    }
+    .Selectable .DayPicker-Day {
+      border-radius: 0 !important;
+    }
+    .Selectable .DayPicker-Day--start {
+      border-top-left-radius: 50% !important;
+      border-bottom-left-radius: 50% !important;
+    }
+    .Selectable .DayPicker-Day--end {
+      border-top-right-radius: 50% !important;
+      border-bottom-right-radius: 50% !important;
+    }
+    .DayPicker-Day[role=gridcell] {
+  border-radius: 50% !important;
+    }
+    .DayPicker-Caption{
+      color: white;
+    }
+    .DayPicker-Weekday{
+      color: lightgoldenrodyellow;
+    }
+    hr{
+      color: lightgoldenrodyellow !important;
+    }
+  `}</style>
+          </Helmet>
+        </div>
+      );
+    }
   }
-  .Selectable .DayPicker-Day {
-    border-radius: 0 !important;
-  }
-  .Selectable .DayPicker-Day--start {
-    border-top-left-radius: 50% !important;
-    border-bottom-left-radius: 50% !important;
-  }
-  .Selectable .DayPicker-Day--end {
-    border-top-right-radius: 50% !important;
-    border-bottom-right-radius: 50% !important;
-  }
-  .DayPicker-Day[role=gridcell] {
-border-radius: 50% !important;
-  }
-  .DayPicker-Caption{
-    color: white;
-  }
-  .DayPicker-Weekday{
-    color: lightgoldenrodyellow;
-  }
-  hr{
-    color: lightgoldenrodyellow !important;
-  }
-`}</style>
-        </Helmet>
-      </div>
-    );
-  }
-}
